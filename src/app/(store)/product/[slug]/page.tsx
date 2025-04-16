f6c663259a007ae7f8d63dbeee5eef34c9f899a2
Davi Marques <davimgcb@gmail.com>
2025-04-15 22:23:15 -0300

    fix: params should be awaited before using the propertis

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -21,7 +21,9 @@ async function getProduct(slug: string): Promise<Product> {
 }
 
 export default async function ProductPage({ params }: ProductProps) {
-  const product = await getProduct(params.slug);
+  const { slug } = await params
+
+  const product = await getProduct(slug);
 
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
